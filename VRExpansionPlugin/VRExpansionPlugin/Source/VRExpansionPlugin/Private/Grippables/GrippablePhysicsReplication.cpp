f45007b6412d12bd64f976d77c2e6e9675ea0a36
Ed94 <edwardgz@gmail.com>
2019-09-06 20:11:10 -0400

    Initial Commit. Original 4.22 Source

---
+++ VRExpansionPlugin/VRExpansionPlugin/Source/VRExpansionPlugin/Private/Grippables/GrippablePhysicsReplication.cpp
@@ -0,0 +1,23 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Grippables/GrippablePhysicsReplication.h"
+#include "UObject/ObjectMacros.h"
+#include "UObject/Interface.h"
+
+// I cannot dynamic cast without RTTI so I am using a static var as a declarative in case the user removed our custom replicator
+// We don't want our casts to cause issues.
+namespace VRPhysicsReplicationStatics
+{
+	static bool bHasVRPhysicsReplication = false;
+}
+
+FPhysicsReplicationVR::FPhysicsReplicationVR(FPhysScene* PhysScene) :
+	FPhysicsReplication(PhysScene)
+{
+	VRPhysicsReplicationStatics::bHasVRPhysicsReplication = true;
+}
+
+bool FPhysicsReplicationVR::IsInitialized()
+{
+	return VRPhysicsReplicationStatics::bHasVRPhysicsReplication;
+}
